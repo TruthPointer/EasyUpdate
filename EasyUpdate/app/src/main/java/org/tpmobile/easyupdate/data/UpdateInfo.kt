76ecdf9c5010f36c2349e4e01b354205b67e00c1
Truth Seeker <OneTruthPointer@outlook.com>
2026-02-28 16:27:45 +0800

    Update UpdateInfo.kt

--- EasyUpdate/app/src/main/java/org/tpmobile/easyupdate/data/UpdateInfo.kt
+++ EasyUpdate/app/src/main/java/org/tpmobile/easyupdate/data/UpdateInfo.kt
@@ -1,13 +1,29 @@
 package org.tpmobile.easyupdate.data
 
+import org.tpmobile.easyupdate.util.ktx.toIntWithDefaultZero
 
-data class UpdateInfo(val sources: MutableList<String>?, val appInfos: MutableList<AppInfo>?) {
+
+data class UpdateInfo(val version: String?, val sources: MutableList<String>?, val appInfos: MutableList<AppInfo>?) {
     fun isUpdateInfoValid(): Boolean = appInfos?.all { it.isAppInfoValid() } ?: false
     fun isSourcesValid(): Boolean = sources?.all { it.isNotEmpty() } ?: false
+
+    fun isAssetsNewerThanCache(assetsVersion: String?, cacheVersion: String? ): Boolean{
+        if(cacheVersion.isNullOrEmpty()) return true
+        if(assetsVersion.isNullOrEmpty()) return false
+        val cacheVers = cacheVersion.split(".").map { it.toIntWithDefaultZero() }
+        val assetsVers = assetsVersion.split(".").map { it.toIntWithDefaultZero() }
+        assetsVers.forEachIndexed { index, i ->
+            if(i == cacheVers[index]) return@forEachIndexed
+            return i > assetsVers[index]
+        }
+        return false
+    }
 }
 
 data class AppInfo(
     val name: String,
+    val description: String?,
+    val withTutorial: Boolean?,
     val urls: MutableList<String>,
 ) {
     fun isAppInfoValid(): Boolean =
